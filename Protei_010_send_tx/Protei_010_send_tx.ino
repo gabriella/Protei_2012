9d1ccddb329daba827461b751f9eebb94747a3c2
gabriella levine <gabriellalevine@gabriellas-MacBook-Air.local>
2012-08-23 22:02:58 -0700

    now i'm using Protei_010 codes, the send one sends 4 bytes and if it receives '1' it turns a light on for signal feedback. also if the signal goes out on the receiving end the boat shuts off until it hears new signal. the receive code receives a signal and tells the right and left motor what to do

---
+++ Protei_010_send_tx/Protei_010_send_tx.ino
@@ -0,0 +1,79 @@
+/*
+PROTEI_010
+ RC-->Xbee-->xbee-->arduino-->motor
+ written by gabriella levine
+ 2012
+ */
+
+// These constants won't change.  They're used to give names
+// to the pins used:
+const int powerPin = 13;
+const int signalPin = 12;
+
+const int JSTK_RIGHT = A0;  // Analog input pin that the potentiometer is attached to
+const int JSTK_LEFT= A5; // Analog output pin that the LED is attached to
+
+int sensorValueRIGHT = 0;        // value read from the pot
+int sensorValueLEFT = 0;        // value read from the pot
+
+int currentMillis = 0;
+int previousMillis = 0;
+
+int signalState = 0;
+
+long interval = 200;
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600); 
+  pinMode(powerPin, OUTPUT);
+  pinMode(JSTK_RIGHT, INPUT);
+  pinMode(JSTK_LEFT, INPUT);
+  pinMode(signalPin, OUTPUT);
+}
+
+void loop() {
+
+  if(Serial.available()>0){
+    if(Serial.read()=='1'){
+      digitalWrite(signalPin, HIGH);
+      signalState = 1;
+    }
+    signalState = 0;
+  }
+  else if(signalState ==0){
+    unsigned long currentMillis = millis();
+    if(currentMillis - previousMillis>interval){
+      previousMillis=currentMillis;
+      digitalWrite(signalPin, LOW);
+    }
+  }
+
+
+
+
+  digitalWrite(powerPin, HIGH);
+  // read the analog in value and map it, 866 to 108
+  sensorValueRIGHT = map(analogRead(JSTK_RIGHT),200,860,0,255);     
+   sensorValueRIGHT= constrain(sensorValueRIGHT,0,255);
+
+  Serial.print('R');
+  delay(10);
+  // Serial.write(outputValue9);  
+  Serial.write(sensorValueRIGHT);  
+  //Serial.write(250);
+  delay(10);
+  //delay(20);
+  sensorValueLEFT = map(analogRead(JSTK_LEFT),200,860,0,255);
+   sensorValueLEFT = constrain(sensorValueLEFT, 0,255);
+
+  Serial.print('L');
+  delay(10);
+  Serial.write(sensorValueLEFT); 
+  delay(10);
+  Serial.flush();  
+}
+
+
+
+
+
---
+++ RECEIVE_2_bytes_FB_____works/RECEIVE_2_bytes_FB_____works.ino
@@ -0,0 +1,97 @@
+/*
+Serial commo from xbee with xbee's pin 2 and 3 connected to rx1, tx1
+ PROTEI_010
+ 2012
+ written by gabriella levine
+ */
+  #include <Metro.h>
+ Metro dataMetro = Metro(2500); 
+
+int incomingData;
+int incomingDataInt;
+const int OUTPUT_1 = 11; // Analog output pin that the Motor1 pwm pin is attached to
+const int DIR_PIN_1=13; //digital output for logic direction pin motor 1
+const int OUTPUT_2= 10; // Analog output pin that the MOTOR2 pwm pin is attached to
+const int DIR_PIN_2 = 12;//digitaloutput for logic direction pin motor2
+
+int sensorState = 0;  //set sensor state to be either 0 (for Left, incoming data 'L') or 1(for Right, incoming data  'R');
+int previousState;
+
+int data;
+void setup() {
+  // initialize serial communications at 9600 bps:
+  pinMode(OUTPUT_1,OUTPUT);
+  pinMode(OUTPUT_2,OUTPUT);
+  pinMode(DIR_PIN_1, OUTPUT);
+  pinMode(DIR_PIN_2, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  while (Serial.available() > 0) {
+    Serial.print('1');
+    byte incomingData = Serial.read();
+    incomingDataInt = (int)incomingData;
+
+    if(incomingData=='L'){
+      sensorState = 1; // go to state 1, waiting for R data
+    } 
+    else  if (incomingData == 'R') {
+      sensorState = 2;
+    } 
+    else if (sensorState == 1) {
+      analogWrite(OUTPUT_1, incomingDataInt);
+      if(incomingDataInt<=100){
+        digitalWrite(DIR_PIN_1, LOW); 
+        analogWrite(OUTPUT_1,map(incomingDataInt, 120,0,0,255));
+      }
+      else if(incomingDataInt>=150){
+        digitalWrite(DIR_PIN_1, HIGH); 
+        analogWrite(OUTPUT_1,map(incomingDataInt, 150,255,0,255));
+      }
+      else{
+        digitalWrite(OUTPUT_1, LOW);
+      }
+      // Serial.println(incomingDataInt);
+      sensorState = 0;
+    }
+
+    else if (sensorState == 2) {
+      analogWrite(OUTPUT_2, incomingDataInt);
+      if(incomingDataInt<=120){
+        digitalWrite(DIR_PIN_2, LOW); 
+        analogWrite(OUTPUT_2,map(incomingDataInt, 120,0,0,255));
+      }
+      else if(incomingDataInt>=150){
+        digitalWrite(DIR_PIN_2, HIGH); 
+        analogWrite(OUTPUT_2,map(incomingDataInt, 150,255,0,255));
+      }
+      else{
+        digitalWrite(OUTPUT_2, LOW);
+      }
+      sensorState =0 ;
+    }
+    
+  
+  }
+  
+
+  
+  delay(20);
+  Serial.flush();
+  
+      if(dataMetro.check()==1){
+if(Serial.available()<1){
+digitalWrite(OUTPUT_1, LOW);
+digitalWrite(OUTPUT_2, LOW);
+
+}
+}
+
+}
+
+
+
+
